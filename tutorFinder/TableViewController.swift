fc8b170ab5efc5f38f54b8475b8f42a1f861b5bb
Baraa Hegazy <hegazyba@msu.edu>
2020-04-15 18:35:27 -0400

    Integrated Tutor table view along with details view into master-branch

--- tutorFinder/TableViewController.swift
+++ tutorFinder/TableViewController.swift
@@ -7,16 +7,96 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class TableViewController: UIViewController {
+class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    var profiles = [PFObject]()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return profiles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TutorCell") as! TutorCell
+        let profile = profiles[indexPath.row]
+        
+        cell.nameLabel.text = profile["name"] as? String
+        cell.descLabel.text = profile["description"] as? String
+        
+        let imageFile = profile["profilePic"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.profileImage.af_setImage(withURL: url)
+        return cell
+    }
+    
 
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
         // Do any additional setup after loading the view.
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let query = PFQuery(className: "Profiles")
+        query.limit = 100
+        query.findObjectsInBackground { (profiles, error) in
+            if profiles != nil {
+                self.profiles = profiles!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+            self.searchBar.showsCancelButton = true
+        self.tableView.reloadData()
+            
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+            searchBar.showsCancelButton = false
+            searchBar.text = ""
+            searchBar.resignFirstResponder()
+            let query = PFQuery(className: "Profiles")
+            query.findObjectsInBackground { (profiles, error) in
+                if profiles != nil {
+                    self.profiles = profiles!
+                    self.tableView.reloadData()
+                }
+            }
+    }
     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("searching now")
+        let query1 = PFQuery(className:"Profiles")
+        let query2 = PFQuery(className:"Profiles")
+        let query3 = PFQuery(className:"Profiles")
+        query1.whereKey("name", contains: searchText)
+        query2.whereKey("description", contains: searchText)
+        query3.whereKey("contactEmail", contains: searchText)
+        // add another query here that has subjects being searched
+        let query = PFQuery.orQuery(withSubqueries: [query1])
+        query.findObjectsInBackground { (profiles,error) in
+            if profiles != nil {
+                self.profiles = profiles!
+            }
+        }
+        tableView.reloadData()
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let profile = profiles[indexPath.row]
+        let detailsViewConroller = segue.destination as! TutorDetailsViewController
+        detailsViewConroller.profile = profile
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
